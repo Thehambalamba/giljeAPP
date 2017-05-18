32bd8babb977c533e72b1f9be0cbc9031ebbc26a
Nikola Pervic <nikolapervic@Nikolas-MacBook-Pro.local>
2017-05-18 19:10:45 +0200

    Revert "Merge pull request #2 from Thehambalamba/login"
    This reverts commit 62dd1c20f5bee29075258d2de0578b2843d4698f, reversing
    changes made to 632365cf0e9238fe2e7fa6a211ba3eb648a5f18f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdGridListModule, MdInputModule } from '@angular/material';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
 
 
 @NgModule({
@@ -22,9 +22,7 @@ import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModu
     MdMenuModule,
     MdCardModule,
     MdToolbarModule,
-    MdIconModule,
-    MdGridListModule,
-    MdInputModule
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
