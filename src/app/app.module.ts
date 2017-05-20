8927389da33bd75421014551ab6cc2d067b276f9
Nikola Pervic <nikolapervic@Nikolas-MacBook-Pro.local>
2017-05-20 16:35:30 +0200

    login-register
    Kreacija login, register i products componenata. Uvođenje rutera i
    kreiranja ruta za komponente. Stilizacija i pravljenje templata za
    login i register.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdListModule } from '@angular/material';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdListModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductsComponent } from './components/products/products.component';
 
+const appRoutes: Routes = [
+  {path:'', component: ProductsComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
     MdButtonModule,
     MdMenuModule,
     MdCardModule,
     MdToolbarModule,
     MdIconModule,
-    MdListModule
+    MdListModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
