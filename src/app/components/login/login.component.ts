93049698ace5f4dc709dfd87c39db97b0692c52c
Nikola Pervic <nikolapervic@Nikolas-MacBook-Pro.local>
2017-05-21 05:56:13 +0200

    service-login-register
    Kreacija servisa za login i register strane. Dodavanje
    angular2-flash-messages paketa. Servisi kreirani su validate i auth.
    Validiranje forma za login i registraciju. Kreiran local storage po
    loginu i pražnjenje istog po logoutu(autentikacija).

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  userName: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+  	private authService: AuthService,
+  	private router: Router,
+  	private flashMessage: FlashMessagesService
+  	) { }
 
   ngOnInit() {
   }
+  onLoginSubmit(){
+  	const user = {
+  		userName: this.userName,
+  		password: this.password
+  	}
 
+  	this.authService.authenticateUser(user).subscribe(data => {
+  		if (data.success) {
+  			this.authService.storeUserData(data.token, data.user);
+  			this.flashMessage.show('Uspešno ste se ulogovali.', {cssClass: '', timeout: 5000});
+  			this.router.navigate(['/']);
+  		} else {
+  			this.flashMessage.show(data.message, {cssClass: '', timeout: 5000});
+  			this.router.navigate(['login']);
+  		}
+
+  	});
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  firstName: String;
+  lastName: String;
+  userName: String;
+  password: String;
+  email: String;
+  phoneNumber: Number;
+
+
+  constructor(
+   private validateService: ValidateService,
+   private flashMessage: FlashMessagesService,
+   private authService: AuthService,
+   private router: Router
+   ) { }
 
   ngOnInit() {
   }
 
+  onRegisterSubmit() {
+  const user = {
+  	firstName: this.firstName,
+  	lastName: this.lastName,
+  	userName: this.userName,
+  	password: this.password,
+  	email: this.email,
+  	phoneNumber: this.phoneNumber,
+
+  }
+
+  // Provera polja
+  if (!this.validateService.validateRegister(user)){
+  	this.flashMessage.show('Molimo vas popunite sva polja.', {cssClass: '',timeout: 3000});
+  	return false;
+  }
+
+  // Provera emaila
+  if (!this.validateService.validateEmail(user.email)){
+  	this.flashMessage.show('Uneti email nije validan.Molimo vas unesite validan email.', {cssClass: '',timeout: 3000});
+  	return false;
+  }
+
+  // Kreiranje korisnika
+  this.authService.registerUser(user).subscribe(data => {
+    if (data.success) {
+     this.flashMessage.show('Uspešno ste se registrovali.Možete se ulogovati.', {cssClass: '',timeout: 3000});
+     this.router.navigate(['/login']);
+    } else {
+     this.flashMessage.show('Došlo je do greške.Molimo vas pokušajte ponovo.', {cssClass: '',timeout: 3000});
+     this.router.navigate(['/register']);
+    }
+
+  });
+
+  }
 }
