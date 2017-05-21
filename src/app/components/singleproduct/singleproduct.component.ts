4f75d3e5000a4aa6aa72720ca247febd0eb93634
Nikola Pervic <nikolapervic@Nikolas-MacBook-Pro.local>
2017-05-22 00:14:03 +0200

    products
    Dodavanje componente za proizvode. Kreacija get product funkcije u
    authServisu. Dodavanje funkcionalnosti slanje jsonweb tokena uz HTTP
    get zahteve za proizvode. Kreiranja single product componente.

---
+++ src/app/components/singleproduct/singleproduct.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-singleproduct',
+  templateUrl: './singleproduct.component.html',
+  styleUrls: ['./singleproduct.component.css']
+})
+export class SingleproductComponent implements OnInit {
+  
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  	
+  }
+
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/map';
 export class AuthService {
   authToken: any;
   user: any;
-
+ 
   constructor(private http: Http) { }
 
   registerUser(user) {
@@ -23,14 +23,35 @@ export class AuthService {
   	 .map(res => res.json());
   }
 
+  getProducts() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type','application/json');
+    return this.http.get('http://localhost:8080/api/products', {headers: headers})
+     .map(res => res.json());
+
+  }
+  getProductsById() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type','application/json');
+    return this.http.get('http://localhost:8080/api/products/${productId}', {headers: headers})
+     .map(res => res.json());
+
+  }
+
   storeUserData(token, user) {
   	localStorage.setItem('id_token', token);
   	localStorage.setItem('user', JSON.stringify(user));
   	this.authToken = token;
   	this.user = user;
-
   }
-
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
   logout() {
   	this.authToken = null;
   	this.user = null;
